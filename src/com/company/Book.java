f5a9f71c3dc3d49d4618fa312c0d888725bc0532
lz212344 <lz212344@reigate.ac.uk>
2022-01-11 15:38:17 +0000

    book class

---
+++ src/com/company/Book.java
@@ -0,0 +1,48 @@
+package com.company;
+
+public class Book {
+
+    private String title;
+    private int ISBN;
+    private String author;
+    private String genre;
+
+    public Book(String title, int ISBN, String author, String genre) {
+        this.title = title;
+        this.ISBN = ISBN;
+        this.author = author;
+        this.genre = genre;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(int ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
