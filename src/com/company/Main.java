b5a1aa4b3d7b5de3ebe96051579d56f394636d11
lz212344 <lz212344@reigate.ac.uk>
2022-01-28 10:58:50 +0000

    register (with admins.txt), arraylist of books to avoid reading the file each time, mainmenu function loop unless user ends it, and changed a few other things. Still need to add books and admins to the txt's (probably in an endProgram function)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,9 +8,14 @@ import java.util.regex.Pattern;
 public class Main {
 
     private static final File library = new File("library.txt");
+    private static final File adminsFile = new File("admins.txt");
+    private static ArrayList<String> books = new ArrayList<>();
+    private static ArrayList<String> admins = new ArrayList<>();
 
     public static void main(String[] args) {
-        createFile();
+        if(!logIn()) return;
+        createFiles();
+        readFile();
         menu();
     }
 
@@ -20,16 +25,14 @@ public class Main {
         return scanner.nextLine();
     }
 
-    public static ArrayList<Book> getBookDetails() {
+    public static void getBookDetails() {
         boolean add = true;
-        ArrayList<Book> addBooks = new ArrayList<>();
         while(add) {
             String bookName = getInput("Enter the title");
             String bookISBN = getInput("Enter the ISBN");
             String bookAuthor = getInput("Enter the author");
             String bookGenre = getInput("Enter the genre");
-            Book book = new Book(bookName, bookISBN, bookAuthor, bookGenre);
-            addBooks.add(book);
+            books.add(bookName + "," + bookISBN + "," + bookAuthor + "," + bookGenre);
             boolean validInput = false;
             while(!validInput) {
                 String addAnother = getInput("Would you like to add another book? (yes/no)");
@@ -43,10 +46,17 @@ public class Main {
                 }
             }
         }
-        return addBooks;
     }
 
-    public static void createFile() {
+    public static void createFiles() {
+        try {
+            if(adminsFile.createNewFile()) {
+                System.out.println("admins.txt did not exist. I have created the file.");
+            }
+        } catch (IOException e) {
+            System.out.println("ERROR");
+            e.printStackTrace();
+        }
         try {
             if(library.createNewFile()) {
                 System.out.println("library.txt did not exist. I have created the file.");
@@ -57,24 +67,23 @@ public class Main {
         }
     }
 
-    public static ArrayList<String> readFile() {
-        ArrayList<String> books = new ArrayList<>();
+    public static void readFile() {
+        ArrayList<String> booksFromFile = new ArrayList<>();
         try {
             Scanner myReader = new Scanner(library);
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
-                books.add(data + "\n");
+                booksFromFile.add(data + "\n");
             }
             myReader.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred while reading the library.txt file.");
             e.printStackTrace();
         }
-        return books;
+        books = booksFromFile;
     }
 
     public static ArrayList<String> formatBooks() {
-        ArrayList<String> books = readFile();
         ArrayList<String> prettyBooks = new ArrayList<>();
         for (String book : books) {
             String[] bookDetails = book.split(",");
@@ -88,49 +97,46 @@ public class Main {
         return prettyBooks;
     }
 
-    public static void writeToFile(ArrayList<Book> books) {
-        try {
-            FileWriter myWriter = new FileWriter(library.getName(), true);
-            for (Book book : books) {
-                myWriter.write(book.getTitle() + "," + book.getISBN() + "," + book.getAuthor() + "," + book.getGenre() + "\n");
-            }
-            myWriter.close();
-            System.out.println("Added books to the library");
-        } catch (IOException e) {
-            System.out.println("An error occurred while writing to the library.txt file.");
-            e.printStackTrace();
-        }
-    }
-
     public static void menu() {
         boolean validInput = false;
-        while(!validInput) {
-            int input = Integer.parseInt(getInput("Would you like to:" +
-                    "\n1. Print the books in the library" +
-                    "\n2. Add books to the library" +
-                    "\n3. Remove a book from the library"));
-            switch (input) {
-                case 1:
-                    System.out.println(formatBooks().size() == 0 ? "No books stored in the library" : String.join("\n", formatBooks()));
-                    validInput = true;
-                    break;
-                case 2:
-                    writeToFile(getBookDetails());
-                    validInput = true;
-                    break;
-                case 3:
-                    deleteBook();
-                    validInput = true;
-                    break;
-                default:
-                    System.out.println("Invalid input");
+        boolean run = true;
+        while(run) {
+            while(!validInput) {
+                int input = Integer.parseInt(getInput("Would you like to:" +
+                        "\n1. Add a new admin to the library" +
+                        "\n2. Print the books in the library" +
+                        "\n3. Add books to the library" +
+                        "\n4. Remove a book from the library" +
+                        "\n5. End the program"));
+                switch (input) {
+                    case 1:
+                        register();
+                        validInput = true;
+                        break;
+                    case 2:
+                        System.out.println(formatBooks().size() == 0 ? "No books stored in the library" : String.join("\n", formatBooks()));
+                        validInput = true;
+                        break;
+                    case 3:
+                        getBookDetails();
+                        validInput = true;
+                        break;
+                    case 4:
+                        deleteBook();
+                        validInput = true;
+                        break;
+                    case 5:
+                        run = false;
+                        break;
+                    default:
+                        System.out.println("Invalid input");
+                }
             }
         }
     }
 
     public static void deleteBook() {
         String name = getInput("Enter the name of the book you want to delete");
-        ArrayList<String> books = readFile();
         System.out.println(books.size());
         System.out.println(name);
         //not finished
@@ -144,19 +150,22 @@ public class Main {
         return true;
     }
 
-    public static Boolean register() {
-        Boolean valid = false;
+    public static void register() {
         String username = getInput("Enter username");
         String password = getInput("Enter password");
         Pattern usernamePattern = Pattern.compile("^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$", Pattern.CASE_INSENSITIVE);
-        Matcher matcher = usernamePattern.matcher(username);
-        if (!matcher.matches()) {
+        Matcher usernameMatcher = usernamePattern.matcher(username);
+        Pattern passwordPattern = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{8,}$", Pattern.CASE_INSENSITIVE);
+        Matcher passwordMatcher = passwordPattern.matcher(password);
+        if (!usernameMatcher.matches()) {
             System.out.println("Invalid email");
-        } else if(password.length() < 8) {
-            // need to finish with more regex
+            return;
+        } else if(!passwordMatcher.matches()) {
+            System.out.println("Invalid password. It must have at least one number, one lower case letter, and one upper case letter");
+            return;
         } else {
-            valid = true;
+            admins.add(username + " " + password);
+            System.out.println("Added admin to library");
         }
-        return valid;
     }
 }
